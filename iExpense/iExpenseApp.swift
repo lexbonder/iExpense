9d28a29e3bed9083bd3fc7b1287eed01043d92fe
Alex Bonder <lexbonder@gmail.com>
2023-07-25 16:20:45 -0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Alex Bonder on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
